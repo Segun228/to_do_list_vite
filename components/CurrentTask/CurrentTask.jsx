b002f69682043d506ee279f7cf74161848fa1560
Segun228 <segunperkele@gmail.com>
2025-04-05 23:14:24 +0300

    added functions

--- components/CurrentTask/CurrentTask.jsx
+++ components/CurrentTask/CurrentTask.jsx
@@ -1,9 +1,11 @@
 const CurrentTask = (props) => {
+    
+    
     return ( 
-        <>
-        <div className="current-task-item">{props.taskText}</div>
-        <button className="delete__button">delete the task</button>
-        </>
+        <div  className="task-container">
+        <div onClick={()=> props.toggleTask(props.id)} className="current-task-item">{props.taskText}</div>
+        <button onClick={()=> props.removeTask(props.id)} className="delete__button">delete the task</button>
+        </div>
     );
 }
 export default CurrentTask;
