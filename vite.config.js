8d3434126a64dea5e43a62754ca79e0cd45a8c68
Segun228 <segunperkele@gmail.com>
2025-04-06 17:53:52 +0300

    correcting vite config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ['tic-tac-toe-adaptive-react.onrender.com', 'localhost'],
+    watch: {
+      usePolling: true,
+    },
+    host: true,
+    strictPort: true,
+    port: 8080
+  }
 })
